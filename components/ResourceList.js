b4e7e13f4ddb868c5e6bf68555642993fdf763a5
profaddy <adnansaify11@gmail.com>
2020-06-08 19:54:49 +0530

    resource list component created that will allow any items to be displayed passing itemList and some functions to handle extra functinalities

--- components/ResourceList.js
+++ components/ResourceList.js
@@ -1,5 +1,3 @@
-import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
 import {
   Card,
   ResourceList,
@@ -7,127 +5,57 @@ import {
   TextStyle,
   Thumbnail,
 } from '@shopify/polaris';
-import store from 'store-js';
-import { Redirect } from '@shopify/app-bridge/actions';
-import { Context } from '@shopify/app-bridge-react';
 
-const GET_PRODUCTS_BY_ID = gql`
-  query getProducts($ids: [ID!]!) {
-    nodes(ids: $ids) {
-      ... on Product {
-        title
-        handle
-        descriptionHtml
-        id
-        images(first: 1) {
-          edges {
-            node {
-              originalSrc
-              altText
-            }
-          }
-        }
-        variants(first: 1) {
-          edges {
-            node {
-              price
-              id
-            }
-          }
-        }
-      }
-    }
-  }
-`;
-const getProductList = gql`
-query getProducts($first: [Int!]!) {
-  nodes(first: $first) {
-    ... on Products {
-      title
-      handle
-      descriptionHtml
-      id
-      images(first: 1) {
-        edges {
-          node {
-            originalSrc
-            altText
-          }
-        }
-      }
-      variants(first: 1) {
-        edges {
-          node {
-            price
-            id
-          }
-        }
-      }
-    }
-  }
-}`
+const ResourceListComponent = (props) => {
+    const {itemList,handleSelectedProduct,selectedItems} = props
+    console.log(itemList);
+//   static contextType = Context;
 
-class ResourceListWithProducts extends React.Component {
-  static contextType = Context;
-
-  render() {
-    const app = this.context;
-    const redirectToProduct = () => {
-      const redirect = Redirect.create(app);
-      redirect.dispatch(
-        Redirect.Action.APP,
-        '/edit-products',
-      );
-    };
+    // const app = this.context;
+    // const redirectToProduct = () => {
+    //   const redirect = Redirect.create(app);
+    //   redirect.dispatch(
+    //     Redirect.Action.APP,
+    //     '/edit-products',
+    //   );
+    // };
 
     const twoWeeksFromNow = new Date(Date.now() + 12096e5).toDateString();
     return (
       <>
-        {/* <Query query={getProductList} variables={{ first: 10 }}>
-        {({ data, loading, error }) => {
-                    console.log(data,loading,error,"graphql params")
-          if (loading) { return <div>Loading…</div>; }
-          if (error) { return <div>{error.message}</div>; }
-          return(<>Success</>);
-        }}
-        </Query> */}
-      <Query query={GET_PRODUCTS_BY_ID} variables={{ ids: store.get('ids') }}>
-        {({ data, loading, error }) => {
-          if (loading) { return <div>Loading…</div>; }
-          if (error) { return <div>{error.message}</div>; }
-          console.log(data);
-          return (
+
+          {/* if (loading) { return <div>Loading…</div>; }
+          if (error) { return <div>{error.message}</div>; } */}
             <Card>
               <ResourceList
                 showHeader
                 resourceName={{ singular: 'Product', plural: 'Products' }}
-                items={data.nodes}
+                items={itemList}
+                selectedItems={selectedItems}
+                onSelectionChange={handleSelectedProduct}
+                selectable
                 renderItem={(item) => {
-                  const media = (
-                    <Thumbnail
-                      source={
-                        item.images.edges[0]
-                          ? item.images.edges[0].node.originalSrc
-                          : ''
-                      }
-                      alt={
-                        item.images.edges[0]
-                          ? item.images.edges[0].node.altText
-                          : ''
-                      }
-                    />
-                  );
-                  const price = item.variants.edges[0].node.price;
+                //   const media = (
+                //     <Thumbnail`
+                //       source={
+                //         item`````````````````````````````````````````````.images.edges[0]
+                //           ? item.images.edges[0].node.originalSrc
+                //           : ''
+                //       }
+                //       alt={
+                //         item.images.edges[0]
+                //           ? item.images.edges[0].node.altText
+                //           : ''
+                //       }
+                //     />
+                //   );
+                  const price = item.variants.price;
                   return (
                     <ResourceList.Item
                       id={item.id}
-                      media={media}
+                      selectable
+                    //   media={media}
                       accessibilityLabel={`View details for ${item.title}`}
-                      onClick={() => {
-                        store.set('item', item);
-                        redirectToProduct();
-                      }
-                      }
                     >
                       <Stack>
                         <Stack.Item fill>
@@ -138,7 +66,7 @@ class ResourceListWithProducts extends React.Component {
                           </h3>
                         </Stack.Item>
                         <Stack.Item>
-                          <p>${price}</p>
+                          <p>{item.price}</p>
                         </Stack.Item>
                         <Stack.Item>
                           <p>Expires on {twoWeeksFromNow} </p>
@@ -149,12 +77,9 @@ class ResourceListWithProducts extends React.Component {
                 }}
               />
             </Card>
-          );
-        }}
-      </Query>
       </>
     );
   }
-}
 
-export default ResourceListWithProducts;
+
+export default ResourceListComponent;
