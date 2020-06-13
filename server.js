006b68085097feaa3d996e4c2a048ff889a6afde
profaddy <adnansaify11@gmail.com>
2020-06-13 10:35:32 +0530

    server code refactored

--- server.js
+++ server.js
@@ -1,31 +1,26 @@
 require('isomorphic-fetch');
 const dotenv = require('dotenv');
 dotenv.config();
-const express = require("express");
 const cors = require('@koa/cors');
-const morgan = require("morgan");
 const bodyParser = require("koa-bodyparser");
-// const mongoose = require("mongoose");
 const Koa = require('koa');
 const next = require('next');
 const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
 const { verifyRequest } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
-const Router = require('koa-router');
 
 const port = parseInt(process.env.PORT, 10) || 3001;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
+import productRouter from "./server/routers/productRouter";
 
 const {
   SHOPIFY_API_SECRET_KEY,
   SHOPIFY_API_KEY,
-  HOST,
 } = process.env;
 app.prepare().then(() => {
   const server = new Koa(app);
-  const router = new Router();
   server.use(session({ sameSite: 'none', secure: true }, server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
 
@@ -52,7 +47,8 @@ app.prepare().then(() => {
     })
   );
   server.use(bodyParser());
-  server.use(router.routes());
+  server.use(productRouter.routes());
+  server.use(productRouter.allowedMethods());
   server.use(verifyRequest());
   server.use(async (ctx) => {
     await handle(ctx.req, ctx.res);
@@ -60,72 +56,9 @@ app.prepare().then(() => {
     ctx.res.statusCode = 200;
     return
   });
-  router.get('/api/v1/:endpoint', async (ctx) => {
-    try {
-      const endpoint = ctx.params.endpoint;
-      const response = await fetch(`https://${ctx.cookies.get('shopOrigin')}/admin/api/2020-04/${endpoint}`, {
-      headers: {
-            "X-Shopify-Access-Token": ctx.cookies.get('accessToken'),
-          },
-        });
-        const result = await response.json();
-      ctx.body = {
-        status: 'success',
-        data: result
-      };
-    } catch (err) {
-      console.log(err)
-    }
-  })
-  router.put('/api/v1/:endpoint', async (ctx) => {
-    try {
-      console.log(ctx.request.body,"ctx response");
-      // const reqData = ctx.request.body;
-      const {editOption,editValue,variants} = ctx.request.body
-      variants.forEach(async (variant) => {
-        const updatedPrice = getUpdatedPrice(editOption,Number(variant.price),Number(editValue));
-        console.log(updatedPrice,"updatedPrice>>");
-        const url =`https://${ctx.cookies.get('shopOrigin')}/admin/api/2020-04/variants/${variant.id}.json`
-        const payload = {
-          variant:{
-            id:variant.id,
-            price:`${updatedPrice}`
-          }
-        }
-        const response = await fetch(url,{    headers: {
-            'Content-type': 'application/json; charset=UTF-8', // Indicates the content 
-          "X-Shopify-Access-Token": ctx.cookies.get('accessToken'),
-        },method:'put',body:JSON.stringify(payload)})
-        const resp = await response.json();
-        console.log(`reuqest completed for ${variant.title}`,resp);
-      })
-      const result = []
-      ctx.body = {
-        status: 'success',
-        data: result
-      };
-    } catch (err) {
-      console.log(err)
-    }
-  })
   server.use(cors())
   server.listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`);
   });
 });
 
-//helpers
-const getUpdatedPrice = (editOption,currentPrice,editValue) => {
-  console.log(editOption,"editOption");
-  switch(editOption){
-    case "changeToCustomValue":
-      return editValue;
-    case "addPriceByAmount":
-      return currentPrice + editValue;
-    case "addPriceByPercentage":
-      const offsetToBeAdded = (currentPrice * editValue)/100
-      return currentPrice + offsetToBeAdded;
-    default:
-      return editValue;
-  }
-}
---
+++ server/routers/productRouter.js
@@ -0,0 +1,80 @@
+'use strict';
+import Router from 'koa-router';
+const router = new Router();
+router.get('/api/v1/:endpoint', async (ctx) => {
+  try {
+    const endpoint = ctx.params.endpoint;
+    const response = await fetch(
+      `https://${ctx.cookies.get('shopOrigin')}/admin/api/2020-04/${endpoint}`,
+      {
+        headers: {
+          'X-Shopify-Access-Token': ctx.cookies.get('accessToken'),
+        },
+      }
+    );
+    const result = await response.json();
+    ctx.body = {
+      status: 'success',
+      data: result,
+    };
+  } catch (err) {
+    console.log(err);
+  }
+});
+router.put('/api/v1/:endpoint', async (ctx) => {
+  try {
+    console.log(ctx.request.body, 'ctx response');
+    const { editOption, editValue, variants } = ctx.request.body;
+    variants.forEach(async (variant) => {
+      const updatedPrice = getUpdatedPrice(
+        editOption,
+        Number(variant.price),
+        Number(editValue)
+      );
+      console.log(updatedPrice, 'updatedPrice>>');
+      const url = `https://${ctx.cookies.get(
+        'shopOrigin'
+      )}/admin/api/2020-04/variants/${variant.id}.json`;
+      const payload = {
+        variant: {
+          id: variant.id,
+          price: `${updatedPrice}`,
+        },
+      };
+      const response = await fetch(url, {
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8', // Indicates the content
+          'X-Shopify-Access-Token': ctx.cookies.get('accessToken'),
+        },
+        method: 'put',
+        body: JSON.stringify(payload),
+      });
+      const resp = await response.json();
+      console.log(`reuqest completed for ${variant.title}`, resp);
+    });
+    const result = [];
+    ctx.body = {
+      status: 'success',
+      data: result,
+    };
+  } catch (err) {
+    console.log(err);
+  }
+});
+export default router;
+
+//helpers
+const getUpdatedPrice = (editOption, currentPrice, editValue) => {
+  console.log(editOption, 'editOption');
+  switch (editOption) {
+    case 'changeToCustomValue':
+      return editValue;
+    case 'addPriceByAmount':
+      return currentPrice + editValue;
+    case 'addPriceByPercentage':
+      const offsetToBeAdded = (currentPrice * editValue) / 100;
+      return currentPrice + offsetToBeAdded;
+    default:
+      return editValue;
+  }
+};
