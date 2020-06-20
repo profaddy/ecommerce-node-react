93e77d6b4fd9b527fc1c5ac20dedf42b00f8d8f9
profaddy <adnansaify11@gmail.com>
2020-06-20 12:44:41 +0530

    removed unwanted compoennets folder and added husky

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import EditCard from '../components/EditCard/EditCard';
-import {editOptions} from "./EditOptions";
-import {styles} from "./styles";
+import { editOptions } from './EditOptions';
+import { styles } from './styles';
 
 const Dashboard = (props) => {
   return (
     <div style={styles.container}>
-<div style={styles.header}>
-    Start Editing Products
-</div>
-      <div key= {new Date()} style={styles.eidtCardContainer}>
+      <div style={styles.header}>Start Editing Products</div>
+      <div key={new Date()} style={styles.eidtCardContainer}>
         {editOptions.map((item) => {
           return (
             <div style={styles.editCardItem}>
